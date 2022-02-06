1545298dd5d7c2bd1fe68841c2a664caf23117b3
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-06 20:07:27 +0530

    add

---
+++ Java_Demo_classes/src/basics/add.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class add {
+
+	public static void main(String[] args) {
+		
+				System.out.println("enter two numbers:");
+				int first=10;
+				int second=40;
+				System.out.println(first+""+second);
+				int sum=first+second;
+				System.out.println("the sum is ="+sum);
+
+			}
+	}
+
