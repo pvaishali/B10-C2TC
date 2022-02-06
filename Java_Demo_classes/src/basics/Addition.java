e9d130e1ec4a715dd7e15bba3eddf3f1a10e17c6
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-06 23:11:33 +0530

    Addition of two numbers asking values from user

---
+++ Java_Demo_classes/src/basics/Addition.java
@@ -0,0 +1,17 @@
+package basics;
+import java.util.Scanner;
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		int a, b, sum;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Number: ");
+		a = sc.nextInt();
+		System.out.println("Enter Second Number: ");
+		b = sc.nextInt();
+		sc.close();
+		sum = a + b;
+		System.out.println("Sum of these numbers: "+sum);
+		}
+}
