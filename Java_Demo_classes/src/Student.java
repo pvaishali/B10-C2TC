f7595e4b58682794f660512f46c2d590286a604b
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-10 19:27:52 +0530

    object

---
+++ Java_Demo_classes/src/Student.java
@@ -0,0 +1,15 @@
+
+public class Student {
+int id;
+String name;
+}
+class TestStudent1{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Student s1 =new Student();
+System.out.println(s1.id);
+System.out.println(s1.name);
+
+	}
+
+}
