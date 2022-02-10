2d37051fd96caf9caca30bc792a68a81cc89d787
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-10 15:03:01 +0530

    package2 access modifier

---
+++ Java_Demo_classes/src/Package1/Aceesmodifier01.java
@@ -0,0 +1,30 @@
+package Package1;
+
+public class Aceesmodifier01 {
+int nos;
+float hours;
+int noc;
+public void displayStudents() {
+	System.out.println("number of students: Aceesmodifier01");
+	
+}
+public void displayHours() {
+	System.out.println("number of hours: Aceesmodifier01");
+	
+}
+public void displayClass() {
+	System.out.println("number of class: Aceesmodifier01");
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Aceesmodifier01 am1= new Aceesmodifier01();
+Aceesmodifier01 am01= new Aceesmodifier01();
+am1.displayStudents();
+am01.displayStudents();
+am01.displayHours();
+am1.displayClass();
+am01.displayClass();
+	}
+
+}
---
+++ Java_Demo_classes/src/Package1/Acesmodifier1.java
@@ -0,0 +1,32 @@
+package Package1;
+
+public class Acesmodifier1 {
+			int nos;
+		float hours;
+		int noc;
+		public void displayStudents() {
+			System.out.println("number of students: Aceesmodifier1");
+			
+		}
+		private void displayHours() {
+			System.out.println("number of hours: Aceesmodifier1");
+			
+		}
+		protected void displayClass() {
+			System.out.println("number of class: Aceesmodifier1");
+			
+		}
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		Acesmodifier1 am1= new Acesmodifier1();
+		Aceesmodifier01 am01= new Aceesmodifier01();
+		am1.displayStudents();
+		am01.displayStudents();
+		am01.displayHours();
+		am1.displayClass();
+		am01.displayClass();
+			}
+}
+	
+
+
