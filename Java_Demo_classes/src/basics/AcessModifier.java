9082e4cc32ba2961ad5426cdffa9c23d18bfb9aa
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-10 11:32:19 +0530

    modifier

---
+++ Java_Demo_classes/src/basics/AcessModifier.java
@@ -0,0 +1,24 @@
+package basics;
+
+public class AcessModifier {
+int nos;
+float hrs;
+int noc;
+void displayStudents() {
+	System.out.println("Number of students");
+}
+void displayHours() {
+	System.out.println("number of hours");
+}
+void displayClass() {
+System.out.println("number of classes");	
+}	
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		AcessModifier am = new AcessModifier();
+		am.displayStudents();
+
+	}
+
+}
