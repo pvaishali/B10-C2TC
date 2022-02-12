124f98ee75070e446792fc2d3871d0b0f8f969fc
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-13 00:14:00 +0530

    taking input from user hello

---
+++ Java_Demo_classes/src/Scanner/hello.java
@@ -0,0 +1,13 @@
+package Scanner;
+import java.util.Scanner;
+public class hello {
+
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.println("enter the string:");
+	String sr=input.nextLine();
+	System.out.println("---- "  + sr+ "------");
+	input.close();
+	
+	
+	}}
