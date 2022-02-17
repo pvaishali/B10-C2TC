1c7423fc46d260543ff96c7a8c0618b5bcb18b15
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-17 22:42:26 +0530

    implementing runnable interface

---
+++ Java_Demo_classes/src/feb17/multi3.java
@@ -0,0 +1,14 @@
+package feb17;
+
+public class multi3 implements Runnable{
+public void run() {
+	System.out.println("thread is running...!!!!...");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+multi3 m1 =new multi3();
+Thread t1=new Thread(m1);
+t1.start();
+	}
+
+}
