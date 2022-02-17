bdd1a06235eda6ef48fbe92a6f150d48909aeec9
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-17 10:56:56 +0530

    simple program using final

---
+++ Java_Demo_classes/src/feb16/bike10.java
@@ -0,0 +1,17 @@
+package feb16;
+public class bike10 {
+	final int speedlimit;
+	bike10(){
+		speedlimit=70;
+		System.out.println(speedlimit);
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+new bike10();
+	}
+
+}
