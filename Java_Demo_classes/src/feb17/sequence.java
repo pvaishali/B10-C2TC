acf85c1d91495bbc7ff36d33ac53355ac1ca83d6
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-17 20:01:24 +0530

    checking the print statement is changing or not

---
+++ Java_Demo_classes/src/feb17/sequence.java
@@ -0,0 +1,21 @@
+package feb17;
+
+public class sequence {
+	void displayStudents() {
+		System.out.println("Number of students");
+	}
+	void displayHours() {
+		System.out.println("number of hours");
+	}
+	void displayClass() {
+	System.out.println("number of classes");	
+	}	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+sequence s=new sequence();
+s.displayClass();
+s.displayHours();
+s.displayStudents();
+	}
+
+}
