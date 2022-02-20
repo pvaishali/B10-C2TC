507ff6fa06c73dd00cc93486b0b59d1838e24018
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-20 10:42:09 +0530

    using try catch finally

---
+++ Java_Demo_classes/src/Feb19/TryCatchFinallyExample.java
@@ -0,0 +1,20 @@
+package Feb19;
+
+public abstract class TryCatchFinallyExample {
+
+	public static void main(String[] args) {
+		
+int[] arr=new int[4];
+try {
+	int i = arr[4];
+	System.out.println("inside try block");
+} 
+catch (ArrayIndexOutOfBoundsException ex) {
+	System.out.println("Eception caught in catch block");
+}
+finally {
+	System.out.println("finally block executed");
+}System.out.println("outside try-catch-finally caluse");
+	}
+
+}
