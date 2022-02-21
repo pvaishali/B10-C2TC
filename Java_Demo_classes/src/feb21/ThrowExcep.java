11e50ea87cf49cc5af9f6a3b5131066a8b7d9167
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-21 20:58:21 +0530

    using static and throw ,try catch

---
+++ Java_Demo_classes/src/feb21/ThrowExcep.java
@@ -0,0 +1,29 @@
+package feb21;
+
+public class ThrowExcep {
+	    static void fun()
+	    {
+	        try
+	        {
+	            throw new NullPointerException("demo");
+	        }
+	        catch(NullPointerException e)
+	        {
+	            System.out.println("Caught inside fun().");
+	            throw e; 
+	        }
+	    }
+	 
+	    public static void main(String args[])
+	    {
+	        try
+	        {
+	            fun();
+	        }
+	        catch(NullPointerException e)
+	        {
+	            System.out.println("Caught in main.");
+	        }
+	    }
+	}
+
