62d653e257ce32a1ce6b18ca66825500a386b950
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-21 18:44:36 +0530

    vote

---
+++ Java_Demo_classes/src/n.java
@@ -0,0 +1,13 @@
+
+public class n {
+	static void canVote(int age){
+		if(age<18)
+		System.out.println("you are not an adult!");
+		else
+		System.out.println("you can vote!");
+		}
+		public static void main (String[] args) {
+		canVote(20);
+		canVote(20);
+		}
+}
