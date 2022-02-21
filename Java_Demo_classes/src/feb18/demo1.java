83305c6ab57a9f9272b9eb5d849853ee6b790cc4
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-21 15:47:48 +0530

    airthmetic exception

--- Java_Demo_classes/src/feb18/demo1.java
+++ Java_Demo_classes/src/feb18/demo1.java
@@ -7,6 +7,12 @@ public class demo1 {
 		
 		int a=10;
 		for(int i=3;i>=0;i--){
-		System.out.println(a/i);
+		try {
+			System.out.println(a/i);
+		} catch (ArithmeticException e) {
+			// TODO Auto-generated catch block
+			System.out.println("e");
+			System.out.println("it is an airthmetic exception program");
+			}
 }
 	}}
