c479930dd7e84d6b2a654178d577626cb50dd75d
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-21 10:28:41 +0530

    length of the array

---
+++ Java_Demo_classes/src/feb20/array.java
@@ -0,0 +1,12 @@
+package feb20;
+
+public class array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int arr[]= {1,2,3,4,5};
+System.out.println("the lenghth of the array =");
+System.out.println(arr.length);
+	}
+
+}
