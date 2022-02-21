d169e5cb1081080632b3e5e8a3eaa17ab27743a2
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-21 18:46:37 +0530

    two

---
+++ Java_Demo_classes/src/feb20/string.java
@@ -0,0 +1,23 @@
+package feb20;
+import java.util.Scanner;
+public class string {
+
+
+public static void main(String[] args) {
+    System.out.println("Type two numbers");
+    sum();
+}
+
+private static void sum(){
+    int a = 0;
+    int b = 0;
+    Scanner scan = new Scanner(System.in);
+    try {
+        a = scan.nextInt();
+        b = scan.nextInt();
+        System.out.println(a+b);
+    } catch (Exception ex) {
+        System.err.println("Type numbers in correct format!");
+        sum();
+    }
+}}
