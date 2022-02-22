8c9dc678d65c0a6f0346c89825e1efbf617a2e81
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-22 19:31:48 +0530

    using length

---
+++ Java_Demo_classes/src/feb22/StringLength.java
@@ -0,0 +1,14 @@
+package feb22;
+import java.util.Scanner;
+public class StringLength {
+	public static void main(String[] args) {
+	String str;
+	int length;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("ENTER A STRING:");
+	str=sc.nextLine();
+	System.out.println("LENGTH OF THE INPUT STRING:"+str.length());
+	
+	}
+
+}
