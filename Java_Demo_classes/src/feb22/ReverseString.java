749ebde1100a064804e2208f28b173eba8bf1f79
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-22 18:49:21 +0530

    reverse a string

---
+++ Java_Demo_classes/src/feb22/ReverseString.java
@@ -0,0 +1,18 @@
+package feb22;
+import java.util.Scanner;
+import java.io.*;
+public class ReverseString {
+	public static void main(String[] args) {
+		String name="raghul";
+		String reversedString="";
+		char ch;
+		System.out.println("original word:");
+		System.out.println("raghul");
+		for(int i=0;i<name.length();i++) {
+			ch=name.charAt(i);
+			reversedString=ch+reversedString;
+		}
+		System.out.println("the reversed word:"+reversedString);
+	}
+
+}
