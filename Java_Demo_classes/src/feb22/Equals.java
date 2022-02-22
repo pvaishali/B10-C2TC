f5e7ba63ac4bea27921f9f159352d6276f78ef83
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-22 19:10:18 +0530

    basic program using equals

---
+++ Java_Demo_classes/src/feb22/Equals.java
@@ -0,0 +1,15 @@
+package feb22;
+
+public class Equals {
+
+	public static void main(String[] args) {
+		String s1="vaishali";
+		String s2="VAISHALI";
+		String s3="raghul";
+		String s4="vaishali";
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equals(s3));
+		System.out.println(s1.equals(s4));
+	}
+
+}
