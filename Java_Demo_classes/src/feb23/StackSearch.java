3c4122a2b292c97483617de4627ca2e8b8eeffd1
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-23 21:37:50 +0530

    stack using search method

---
+++ Java_Demo_classes/src/feb23/StackSearch.java
@@ -0,0 +1,13 @@
+package feb23;
+import java.util.Stack;
+public class StackSearch {
+	public static void main(String[] args) {
+		Stack<String>s1=new Stack<String>();
+		s1.push("raghul");
+		s1.push("ganesh");
+System.out.println("the stack is:"+s1);
+System.out.println("does the stack contain:"+s1.search("ganesh")); 
+System.out.println("does the stack contain:"+s1.search("vaishu")); 
+	}
+
+}
