392614f26da286dc9f7b4da03b4642d810d4be64
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-24 21:23:10 +0530

    array using  compare(array 1, array 2) Method

---
+++ Java_Demo_classes/src/feb23/ArrayDemo2.java
@@ -0,0 +1,12 @@
+package feb23;
+import java.util.Arrays;
+public class ArrayDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int intArr[] = { 10, 20, 15, 22, 35 };
+	        int intArr1[] = { 10, 15, 22 };
+	        System.out.println("Integer Arrays on comparison: "  + Arrays.compare(intArr, intArr1));
+	}
+
+}
