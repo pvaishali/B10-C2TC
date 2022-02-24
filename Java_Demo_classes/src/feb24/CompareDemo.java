19f21d8151dc5788b1ef31df5922c0788a7169c1
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-02-24 21:04:41 +0530

    comparator demo 1

---
+++ Java_Demo_classes/src/feb24/CompareDemo.java
@@ -0,0 +1,11 @@
+package feb24;
+public class CompareDemo {
+	public static void main(String[] args) {
+	String s1="d";
+	String s2="a";
+	System.out.println("result:"+s1.compareTo(s2));
+	
+
+	}
+
+}
