1e14cd1b13a28be4bd3debeca124821b20b2b497
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-03-02 10:32:55 +0530

    sample connection

---
+++ java_jdbc_classes/src/jdbc/jdbcDemo.java
@@ -0,0 +1,24 @@
+package jdbc;
+
+import java.sql.*;
+
+
+public class jdbcDemo {
+	public static void main(String[] args) throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/sample","root","latchu20");
+		
+    Statement st=con.createStatement();
+//String query="create table student(id int primary key,sname varchar(20))";
+   // String query1="insert into student values(1,'vaishu')";
+  //  String query2="insert into student values(2,'papaa')";
+    //String query3="insert into student values(3,'gud')";
+   // String query3="delete from student where id=3";
+  String query2="update student set sname='raghul' where id=2";  
+st.executeUpdate(query2);
+System.out.println("table created");
+System.out.println("inserted");
+st.close();
+con.close();
+}
+}
