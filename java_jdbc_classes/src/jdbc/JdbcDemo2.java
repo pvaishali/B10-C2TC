957c1494749b2135faa535e301448642db9b80bb
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-03-02 13:52:44 +0530

    commit connnection

--- java_jdbc_classes/src/jdbc/JdbcDemo2.java
+++ java_jdbc_classes/src/jdbc/JdbcDemo2.java
@@ -10,10 +10,10 @@ public class JdbcDemo2 {
 	//	String query="create table employee(id int primary key,ename varchar(20),esalary int(5))";
 		//String query1="insert into employee values(1,'vaishu','345')";
 		//String query2="insert into employee values(2,'vaishu','455')";
-		String query2="update employee set ename='ganesh' where id=2"; 
+		//String query2="update employee set ename='ganesh' where id=2"; 
 		//st.executeUpdate(query2);
 		//System.out.println("table created");
-	//	System.out.println("inserted");
+		System.out.println("inserted");
 		st.close();
 		con.close();
 		
