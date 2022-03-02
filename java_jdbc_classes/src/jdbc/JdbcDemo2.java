ff38a4665dd611fecabb161911189616df072c14
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-03-02 12:45:13 +0530

    connection

---
+++ java_jdbc_classes/src/jdbc/JdbcDemo2.java
@@ -0,0 +1,23 @@
+package jdbc;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+public class JdbcDemo2 {
+	public static void main(String[] args) throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/raghul","root","latchu20");
+		Statement st=con.createStatement();
+	//	String query="create table employee(id int primary key,ename varchar(20),esalary int(5))";
+		//String query1="insert into employee values(1,'vaishu','345')";
+		//String query2="insert into employee values(2,'vaishu','455')";
+		String query2="update employee set ename='ganesh' where id=2"; 
+		st.executeUpdate(query2);
+		//System.out.println("table created");
+		System.out.println("inserted");
+		st.close();
+		con.close();
+		
+		}
+	}
+
+
