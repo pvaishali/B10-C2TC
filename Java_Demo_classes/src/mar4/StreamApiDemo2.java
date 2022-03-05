11afdef3f2e0be88e3624a8716aca1f4a11f673e
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-03-05 19:41:27 +0530

    using inner class

---
+++ Java_Demo_classes/src/mar4/StreamApiDemo2.java
@@ -0,0 +1,31 @@
+package mar4;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+	//Consumer Interface 
+	class A implements Consumer<Integer>{
+		 public void accept(Integer i) {
+			  System.out.println(i);
+		  }
+	}
+	
+		public class StreamApiDemo2 {
+		public static void main(String[] args) {
+			List<Integer> values = Arrays.asList(1,2,3);
+
+		values.forEach(i-> System.out.println(i));//Lamda Expression
+
+		//Anonymous method
+			for(int i : values) {
+					values.forEach(new Consumer<Integer>(){
+				public void accept(Integer i) {
+					System.out.println(i);
+				}
+			});
+	    }
+			Consumer<Integer> c = new  A();
+			values.forEach(c);
+	}
+	}
+
