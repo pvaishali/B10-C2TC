ba19d364e65025dc1218da405c1c0ee98dd9b6f2
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-03-06 19:22:45 +0530

    Serializable interface

---
+++ Java_Demo_classes/src/mar3/Test.java
@@ -0,0 +1,42 @@
+package mar3;
+import java.io.*;
+class A implements Serializable
+{
+    int i;
+    String s;
+  
+    // A class constructor
+    public A(int i,String s)
+    {
+        this.i = i;
+        this.s = s;
+    }
+}
+  
+public class Test {
+
+	  
+	
+	    public static void main(String[] args)
+	      throws IOException, ClassNotFoundException
+	    {
+	        A a = new A(20,"lakshmi");
+	  
+	        // Serializing 'a'
+	        FileOutputStream fos = new FileOutputStream("xyz.txt");
+	        ObjectOutputStream oos = new ObjectOutputStream(fos);
+	        oos.writeObject(a);
+	  
+	        // De-serializing 'a'
+	        FileInputStream fis = new FileInputStream("xyz.txt");
+	        ObjectInputStream ois = new ObjectInputStream(fis);
+	        A b = (A)ois.readObject();//down-casting object
+	  
+	        System.out.println(b.i+" "+b.s);
+	  
+	        // closing streams
+	        oos.close();
+	        ois.close();
+	    }
+	}
+
