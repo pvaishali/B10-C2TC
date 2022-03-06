d4938eaa5c09c3235dd5bf1c47d9a54db86086fd
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-03-06 14:03:46 +0530

    Equals

---
+++ Java_Demo_classes/src/com/Equals/EqualsDemo1.java
@@ -0,0 +1,19 @@
+package com.Equals;
+public class EqualsDemo1 {
+	public static void main(String[] args) {
+	int r1=8;
+	int r2=2;
+	int r3=8;
+	if(r1==r2) {
+		System.out.println("equal");
+	}else {
+		System.out.println(" not equal");
+	}
+	if(r1==r3) {
+		System.out.println("equal");
+	}else {
+		System.out.println(" not equal");
+	}
+	}
+
+}
