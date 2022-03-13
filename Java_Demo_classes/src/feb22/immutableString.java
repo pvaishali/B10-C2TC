ef11951a38e50f60357db3b87e0149047a55ddc9
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-03-13 21:04:22 +0530

    immutable string

---
+++ Java_Demo_classes/src/feb22/immutableString.java
@@ -0,0 +1,11 @@
+package feb22;
+
+public class immutableString {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String s="Sachin";  
+		   s=s.concat(" Tendulkar");  
+		   System.out.println(s);  
+	}
+
+}
