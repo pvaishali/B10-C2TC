4ce8d16c05a79d63d4bb5f7b0987d2cebc14df3a
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-03-27 21:49:49 +0530

    Applets

---
+++ Applets/src/com/applets/Applets1.java
@@ -0,0 +1,12 @@
+package com.applets;
+
+import java.applet.Applet;
+import java.awt.Button;
+
+public class Applets1 extends Applet{
+Button b;
+public void init() {
+	b=new Button("love");
+	add(b);
+}
+}
