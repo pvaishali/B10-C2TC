4ee233cb35c4259939ed5e6a88c6e758ac292434
Vaishali <Vaishali@DESKTOP-MCAI5MI>
2022-03-27 21:14:29 +0530

    changes

--- JPA_DEMO/src/com/cg/jpacurd/client/Client.java
+++ JPA_DEMO/src/com/cg/jpacurd/client/Client.java
@@ -9,25 +9,25 @@ public class Client {
 		StudentService service = new StudentServiceImpl();
 		Student student = new Student();
 		// Create Operation
-		student.setStudentId(2);
-		student.setName("vaishu");
+		student.setStudentId(4);
+		student.setName("zubbu");
 		service.addStudent(student);
 		//at this breakpoint, we have added one record to table
 		// Retrieve Operation
-		student = service.findStudentById(1);
+		student = service.findStudentById(4);
 		System.out.print("ID:"+student.getStudentId());
 		System.out.println(" Name:"+student.getName()); 
 		// Update Operation
-		student = service.findStudentById(2);
-		student.setName("vaishu papu");
+		student = service.findStudentById(4);
+		student.setName("smart");
 		service.updateStudent(student);
 		//at this breakpoint, we have updated record added in first section
-		student = service.findStudentById(2);
+		student = service.findStudentById(4);
 		System.out.print("ID:"+student.getStudentId());
 		System.out.println(" Name:"+student.getName()); 
 		//at this breakpoint, record is removed from table
 		// Delete Operation
-		student = service.findStudentById(2);
+		student = service.findStudentById(4);
 		//service.removeStudent(student);
 		System.out.println("End of program/Life cycle completed..."); 
 		}
